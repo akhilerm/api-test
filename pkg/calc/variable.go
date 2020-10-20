b4b5ff2fd37d57616e8fce5de8d4d364c2c6a5fc
Akhil Mohan <akhil.mohan@mayadata.io>
2020-10-20 15:52:54 +0530

    add new variables to pkg

--- pkg/calc/variable.go
+++ pkg/calc/variable.go
@@ -5,19 +5,25 @@ import "fmt"
 type Variables struct {
 	X int
 	Y int
+	Z int
 }
 
-func NewVariables(x, y int) *Variables {
+func NewVariables(x, y, z int) *Variables {
 	return &Variables{
 		X: x,
 		Y: y,
+		Z: z,
 	}
 }
 
 func (v *Variables) GetFormattedString() string {
-	return fmt.Sprintf("X: %d, Y: %d", v.X, v.Y)
+	return fmt.Sprintf("X: %d, Y: %d, Z: %d", v.X, v.Y, v.Z)
 }
 
 func (v *Variables) Add() int {
-	return v.X + v.Y
+	return v.X + v.Y + v.Z
+}
+
+func (v *Variables) Multiply() int {
+	return v.X * v.Y * v.Z
 }
