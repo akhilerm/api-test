b901b6587ed9af3b72712411afb138179b241312
Akhil Mohan <akhil.mohan@mayadata.io>
2020-10-20 15:58:25 +0530

    updated samples

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/akhilerm/api-test/pkg/calc"
+	calcv2 "github.com/akhilerm/api-test/v2/pkg/calc"
 )
 
 func main() {
-	a := calc.NewVariables(10, 11)
+	a := calcv2.NewVariables(10, 11, 12)
 	fmt.Printf("Formatted variables = (%s) \n", a.GetFormattedString())
 }
